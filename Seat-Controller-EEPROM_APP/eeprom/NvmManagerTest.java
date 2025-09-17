8ede01a1fdf50d1f954547bb248a12c67aa12094
Salmaakhalledd <salmaakhalledd@gmail.com>
2025-09-17 10:50:17 +0300

    Update NvmManagerTest.java

--- Seat-Controller-EEPROM_APP/eeprom/NvmManagerTest.java
+++ Seat-Controller-EEPROM_APP/eeprom/NvmManagerTest.java
@@ -1,1 +1,23 @@
+@@ -0,0 +1,22 @@
+package eeprom;
 
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class NvmManagerTest {
+
+    @Test
+    public void testReadStartup() {
+        NvmManager nvm = new NvmManager();
+        String res = nvm.readStartup();
+        assertTrue(res.contains("READ_STARTUP"));
+    }
+
+    @Test
+    public void testWriteStartup() {
+        NvmManager nvm = new NvmManager();
+        byte[] data = {0x01, 0x02, 0x03};
+        String res = nvm.writeStartup(data);
+        assertTrue(res.contains("WRITE_STARTUP"));
+    }
+}
